8b979e53350827195a34b6303a0c53b114865124
Florencia Baltar <gabriel.m.ledesma96@gmail.com>
2024-02-01 15:32:43 -0300

    arreglo windowWidth

--- src/app/components/navBar/NavBar.jsx
+++ src/app/components/navBar/NavBar.jsx
@@ -5,22 +5,50 @@ import NavBarDesktop from "./components/NavBarDesktop";
 import NavBarMobile from "./components/NavBarMobile";
 
 const NavBar = () => {
-    const [windowWhidth, setWindowWidth] = useState(window.innerWidth)
+    // const [windowWhidth, setWindowWidth] = useState(window.innerWidth)
 
-    useEffect(() => {
-        const handleResize = () => {
-          setWindowWidth(window.innerWidth);
-        };
+    // useEffect(() => {
+    //     const handleResize = () => {
+    //       setWindowWidth(window.innerWidth);
+    //     };
     
-        window.addEventListener('resize', handleResize);
+    //     window.addEventListener('resize', handleResize);
     
-        return () => {
-          window.removeEventListener('resize', handleResize);
-        };
-      }, []);
+    //     return () => {
+    //       window.removeEventListener('resize', handleResize);
+    //     };
+    //   }, []);
+    function useWindowSize() {
+
+      const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+      });
+    
+      useEffect(() => {
+    
+        function handleResize() {
+        
+          setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+          });
+        }
+
+        window.addEventListener("resize", handleResize);
+         
+        handleResize();
+      
+        return () => window.removeEventListener("resize", handleResize);
+      }, []); 
+      return windowSize;
+    }
+
+    const size = useWindowSize();
+
   return (
     <>
-        {windowWhidth > 1024 ? <NavBarDesktop/> : <NavBarMobile/>}
+        {size.width > 1024 ? <NavBarDesktop/> : <NavBarMobile/>}
     </>
   )
 }
