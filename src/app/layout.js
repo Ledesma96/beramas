bc34d6afcdd32db821ed292967134fac1e1c0fa3
Florencia Baltar <gabriel.m.ledesma96@gmail.com>
2024-02-08 11:06:57 -0300

    cambio de color

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Bera positivo',
   description: 'Explora nuestra colección de sofás y mesas diseñados para elevar el confort y el estilo de tu hogar. Descubre sofisticados diseños y calidad excepcional que transformarán tu sala de estar en un espacio acogedor y moderno. Encuentra el sofá y la mesa perfectos para tu hogar hoy mismo.',
-  keywords: 'Sofás y Mesas, Muebles para el Hogar, Decoración de Interiores, Diseño de Interiores, Mobiliario Moderno, Tienda de Muebles Online, Ofertas de Muebles, Envío Gratuito de Muebles, Confort y Estilo para tu Hogar, Experiencia de Compra Online, Envios al interior, Sofás, Sillones, Esquineros, Sillas, Mesas, Mesas y sillas, Deco y hogar'
+  keywords: 'Bera, Berazategui, Muebles, Sofás, Berazategui muebles, Muebles Berazategui, Sofás y Mesas, Muebles para el Hogar, Decoración de Interiores, Diseño de Interiores, Mobiliario Moderno, Tienda de Muebles Online, Ofertas de Muebles, Envío Gratuito de Muebles, Confort y Estilo para tu Hogar, Experiencia de Compra Online, Envios al interior, Sofás, Sillones, Esquineros, Sillas, Mesas, Mesas y sillas, Deco y hogar'
 }
 
 export default function RootLayout({ children }) {
