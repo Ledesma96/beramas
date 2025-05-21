cc752d1637524235aad0b5997a0594ae6f629619
Gabriel <gabriel.m.ledesma96@gmail.com>
2025-05-21 12:17:51 -0300

    cambio color y logo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['tiendaliving.com.ar','127.0.0.1'],
+        domains: ['tiendaliving.com.ar','127.0.0.1', 'https://berapositivo.onrender.com/'],
       },
 }
 
