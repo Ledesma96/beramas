d44e5e1b3ff436d08757b7f966e899c1a862ef22
Gabriel <gabriel.m.ledesma96@gmail.com>
2025-11-10 15:38:21 -0300

    react icons

--- src/app/detail/[detail]/compnents/Popup.jsx
+++ src/app/detail/[detail]/compnents/Popup.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import Link from 'next/link'
-import { FaWhatsapp } from 'react-icons/fa'
+import { FaWhatsapp } from "react-icons/fa";
 
 const Popup = ({ productName, setShowPopup }) => {
     const message = encodeURIComponent(
